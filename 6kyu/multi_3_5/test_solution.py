eed97a35161991cae7930eeed6defadda2c0e4c7
Pawel Ejsmont <piotr.akanowicz@gmail.com>
2018-03-05 16:22:07 +0100

    Adding simple test cases

--- 6kyu/multi_3_5/test_solution.py
+++ 6kyu/multi_3_5/test_solution.py
@@ -3,5 +3,12 @@ from .mult_3_5 import solution
 
 
 class TestSolution(TestCase):
-    def test_solution(self):
+    def test_solution_for_10(self):
         self.assertEquals(solution(10), 23)
+
+    def test_solution_for_12(self):
+        self.assertEquals(solution(12), 33)
+
+    def test_solution_for_1_and_0(self):
+        self.assertEquals(solution(1), 0)
+        self.assertEquals(solution(0), 0)

12113d8bcb3cdad930353833163311c17fb8abaf
Pawel Ejsmont <piotr.akanowicz@gmail.com>
2018-03-26 11:10:42 +0200

    Initial commit, requirements, documentation

---
+++ 6kyu/longst_substr/longst_substr.py
@@ -0,0 +1,24 @@
+"""
+
+Kata - Longest alphabetical substring
+
+    Find the longest substring in alphabetical order.
+
+    Eg: the longest alphabetical substring in asdfaaaabbbbcttavvfffffdf is aaaabbbbctt.
+
+    There are tests with strings up to 10000 characters long so your code will need to be efficient.
+
+    The input will only consist of lowercase characters and will be at least one letter long.
+"""
+
+__author__ = 'pejs and jejs'
+
+
+def longest(s):
+    """
+    Takes a lowercase string and calculates the longest substring in alphabetical order
+    :param s: a lowercase string at least one letter long
+    :return: the longest substring of the input string in alphabetical order
+    """
+
+    pass
